d75d3a84c5857682b598856b110e878ecc1ce41b
mengyanz <u6015325@anu.edu.au>
2021-12-17 09:22:27 +0000

    add simulator

---
+++ simulator.py
@@ -0,0 +1,17 @@
+def propensity_predictor(context, exists):
+    """learn propensity score
+    """
+    pass
+
+
+def ips_simulator():
+    """reweigh training samplings using ips
+    simulator for user-choice 
+    """
+    pass
+
+
+def generate_rewards():
+    """generate and save simulated rewards on testing data
+    """
+    pass
