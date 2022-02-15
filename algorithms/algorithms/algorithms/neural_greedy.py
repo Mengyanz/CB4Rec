336b91c037b37f1dbc68de2a79c9641a75cd3422
hzy-hzy <837685486@qq.com>
2022-02-15 08:16:49 +0000

    latest

---
+++ algorithms/algorithms/algorithms/neural_greedy.py
@@ -0,0 +1,154 @@
+"""Define a NRMS based greedy recommendation policy. """
+
+import math 
+import numpy as np 
+from collections import defaultdict
+import torch 
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from core.contextual_bandit import ContextualBanditLearner 
+from algorithms.nrms_model import NRMS_Model
+from utils.data_util import read_data, NewsDataset, UserDataset, TrainDataset, load_word2vec, load_cb_topic_news, SimEvalDataset, SimEvalDataset2, SimTrainDataset
+
+class SingleStageNeuralGreedy(ContextualBanditLearner):
+    def __init__(self,device, args, rec_batch_size = 1, pretrained_mode=True, name='SingleStageNeuralGreedy'):
+        """Use NRMS model. 
+            Args:
+                rec_batch_size: int, recommendation size. 
+                n_inference: int, number of Monte Carlo samples of prediction. 
+                pretrained_mode: bool, True: load from a pretrained model, False: no pretrained model 
+        """
+        super(SingleStageNeuralGreedy, self).__init__(args, rec_batch_size, pretrained_mode, name) 
+        self.name = name 
+        self.device = device 
+
+        # preprocessed data 
+        self.nid2index, word2vec, self.nindex2vec = load_word2vec(args)
+        topic_news = load_cb_topic_news(args) # dict, key: subvert; value: list nIDs 
+        cb_news = []
+        for k,v in topic_news.items():
+            cb_news.append(l.strip('\n').split("\t")[0] for l in v) # get nIDs 
+        cb_news = [item for sublist in cb_news for item in sublist]
+        # DEBUG:
+        print('Warning: for debug, sample 1000 candidates news! Remove this line for full evaluation!')
+        cb_news = np.random.choice(cb_news, size=1000, replace=False).tolist()
+        self.cb_news=cb_news
+
+        # model 
+        self.model = NRMS_Model(word2vec).to(self.device)
+
+    def construct_trainable_samples(self):
+        """construct trainable samples which will be used in NRMS model training
+        from self.h_contexts, self.h_actions, self.h_rewards
+        """
+        tr_samples = []
+
+        for i, l in enumerate(self.data_buffer):
+            poss, negs, his, uid, tsp = l
+            if len(poss) > 0 and len(negs) > 0:  # TODO: change when use BCE
+                for pos in poss:
+                    tr_samples.append([pos, negs, his, uid, tsp])
+        return tr_samples
+
+    def train(self):
+        
+        # update learner
+        optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
+        ft_sam = self.construct_trainable_samples()
+        if len(ft_sam) > 0:
+            print('Updating the internal model of the bandit!')
+            ft_ds = SimTrainDataset(self.args, ft_sam, self.nid2index, self.nindex2vec)
+            ft_dl = DataLoader(ft_ds, batch_size=self.args.batch_size, shuffle=True, num_workers=0)
+            
+            # do one epoch only
+            loss = 0
+            self.model.train()
+            ft_loader = tqdm(ft_dl)
+            for cnt, batch_sample in enumerate(ft_loader):
+                candidate_news_index, his_index, label = batch_sample
+                sample_num = candidate_news_index.shape[0]
+                candidate_news_index = candidate_news_index.to(self.device)
+                his_index = his_index.to(self.device)
+                label = label.to(self.device)
+                bz_loss, y_hat = self.model(candidate_news_index, his_index, label)
+
+                loss += bz_loss.detach().cpu().numpy()
+                optimizer.zero_grad()
+                bz_loss.backward()
+
+                optimizer.step()  
+        else:
+            print('Skip update cb learner due to lack valid samples!')
+
+    def update(self, topics, items, rewards, mode = 'item'):
+        """Update its internal model. 
+
+        Args:
+            topics: list of `rec_batch_size` str
+            items: a list of `rec_batch_size` item index (not nIDs, but its numerical index from `nid2index`) 
+            rewards: a list of `rec_batch_size` {0,1}
+            mode: `topic`/`item`
+
+        @TODO: they recommend `rec_batch_size` topics 
+            and each of the topics they recommend an item (`rec_batch_size` items in total). 
+            What if one item appears more than once in the list of `rec_batch_size` items? 
+        """
+        if mode == 'item':
+            self.train() 
+
+    def item_rec(self, uid, cand_news, m = 1): 
+        """
+        Args:
+            uid: str, a user id 
+            cand_news: a list of int (not nIDs but their index version from `nid2index`) 
+            m: int, number of items to rec 
+
+        Return: 
+            items: a list of `len(uids)`int 
+        """
+        batch_size = min(self.args.max_batch_size, len(cand_news))
+
+        # get user vect 
+     
+        h = self.clicked_history[uid]
+        h = h + [0] * (self.args.max_his_len - len(h))
+        h = self.nindex2vec[h]
+
+        h = torch.Tensor(h[None,:,:])
+        sed = SimEvalDataset2(self.args, cand_news, self.nindex2vec)
+        rdl = DataLoader(sed, batch_size=batch_size, shuffle=False, num_workers=4) 
+
+        scores = []
+        for cn in rdl:
+            score = self.model.forward(cn[:,None,:].to(self.device), h.repeat(cn.shape[0],1,1).to(self.device), None, compute_loss=False)
+            scores.append(score.detach().cpu().numpy()) 
+        scores = np.concatenate(scores).squeeze(-1)
+        # print(scores.shape)   
+
+        nid_argmax = np.argsort(scores)[::-1][:m].tolist() # (len(uids),)
+        rec_itms = [cand_news[n] for n in nid_argmax]
+        return rec_itms 
+
+    def sample_actions(self, uids): 
+        """Choose an action given a context. 
+        Args:
+            uids: a list of str uIDs. 
+
+        Return: 
+            topics: (len(uids), `rec_batch_size`)
+            items: (len(uids), `rec_batch_size`) 
+            numbers of items? 
+        """
+
+        # print(self.nid2index)
+        # cand_news = []
+        # for n in cb_news:
+        #     cand_news.append(self.nid2index[n])
+        cand_news = [self.nid2index[n] for n in self.cb_news]
+        rec_items = self.item_rec(uids, cand_news, self.rec_batch_size)
+
+        
+        return np.empty(0), rec_items
+    
---
+++ algorithms/algorithms/algorithms/nrms_model.py
@@ -0,0 +1,340 @@
+from pathlib import Path
+import time
+import datetime
+from tqdm import tqdm
+from collections import defaultdict, Counter
+import copy
+import random
+import re
+import numpy as np
+import os
+from sklearn.metrics import roc_auc_score
+import pickle
+from datetime import datetime 
+import math
+# import uncertainty_toolbox as utc
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_k):
+        super(ScaledDotProductAttention, self).__init__()
+        self.d_k = d_k
+
+    def forward(self, Q, K, V, attn_mask=None):
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(self.d_k)
+        scores = torch.exp(scores)
+        if attn_mask is not None:
+            scores = scores * attn_mask
+        attn = scores / (torch.sum(scores, dim=-1, keepdim=True)  + 1e-8)
+        
+        context = torch.matmul(attn, V)
+        return context, attn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads, d_k, d_v):
+        super(MultiHeadAttention, self).__init__()
+        self.d_model = d_model # 300
+        self.n_heads = n_heads # 20
+        self.d_k = d_k # 20
+        self.d_v = d_v # 20
+        
+        self.W_Q = nn.Linear(d_model, d_k * n_heads) # 300, 400
+        self.W_K = nn.Linear(d_model, d_k * n_heads) # 300, 400
+        self.W_V = nn.Linear(d_model, d_v * n_heads) # 300, 400
+        
+        self._initialize_weights()
+                
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+                
+    def forward(self, Q, K, V, attn_mask=None):
+        #       Q, K, V: [bz, seq_len, 300] -> W -> [bz, seq_len, 400]-> q_s: [bz, 20, seq_len, 20]
+        batch_size, seq_len, _ = Q.shape
+        
+        q_s = self.W_Q(Q).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
+        k_s = self.W_K(K).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
+        v_s = self.W_V(V).view(batch_size, -1, self.n_heads, self.d_v).transpose(1,2)
+        
+        if attn_mask is not None:
+            attn_mask = attn_mask.unsqueeze(1).expand(batch_size, seq_len, seq_len) 
+            attn_mask = attn_mask.unsqueeze(1).repeat(1, self.n_heads, 1, 1) 
+        
+        context, attn = ScaledDotProductAttention(self.d_k)(q_s, k_s, v_s, attn_mask) 
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.n_heads * self.d_v) 
+        return context 
+
+class AdditiveAttention(nn.Module):
+    ''' AttentionPooling used to weighted aggregate news vectors
+    Arg: 
+        d_h: the last dimension of input
+    '''
+    def __init__(self, d_h, hidden_size=200):
+        super(AdditiveAttention, self).__init__()
+        self.att_fc1 = nn.Linear(d_h, hidden_size)
+        self.att_fc2 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x, attn_mask=None):
+        """
+        Args:
+            x: batch_size, candidate_size, candidate_vector_dim
+            attn_mask: batch_size, candidate_size
+        Returns:
+            (shape) batch_size, candidate_vector_dim
+        """
+        bz = x.shape[0]
+        e = self.att_fc1(x)
+        e = nn.Tanh()(e)
+        alpha = self.att_fc2(e)
+
+        alpha = torch.exp(alpha)
+        if attn_mask is not None:
+            alpha = alpha * attn_mask.unsqueeze(2)
+        alpha = alpha / (torch.sum(alpha, dim=1, keepdim=True) + 1e-8)
+
+        x = torch.bmm(x.permute(0, 2, 1), alpha)
+        x = torch.reshape(x, (bz, -1))  # (bz, 400)
+        return x
+
+class TextEncoder(nn.Module):
+    def __init__(self, 
+                 embedding_matrix,
+                 word_embedding_dim=300, 
+                 num_attention_heads=20,
+                 query_vector_dim=200,
+                 dropout_rate=0.2,
+                 enable_gpu=True):
+        super(TextEncoder, self).__init__()
+        self.dropout_rate = dropout_rate
+        pretrained_news_word_embedding = torch.from_numpy(embedding_matrix).float()
+        
+        self.word_embedding = nn.Embedding.from_pretrained(
+            pretrained_news_word_embedding, freeze=False)
+        
+        self.multihead_attention = MultiHeadAttention(word_embedding_dim,
+                                              num_attention_heads, 20, 20)
+        self.additive_attention = AdditiveAttention(num_attention_heads*20,
+                                                    query_vector_dim)
+    def forward(self, text):
+        # REVIEW: remove training=self.training to enable dropout during testing 
+        text_vector = F.dropout(self.word_embedding(text.long()),
+                                p=self.dropout_rate,
+                                # training=self.training
+                                )
+        multihead_text_vector = self.multihead_attention(
+            text_vector, text_vector, text_vector)
+        multihead_text_vector = F.dropout(multihead_text_vector,
+                                          p=self.dropout_rate,
+                                        #   training=self.training
+                                          )
+        # batch_size, word_embedding_dim
+        text_vector = self.additive_attention(multihead_text_vector)
+        return text_vector
+
+class UserEncoder(nn.Module):
+    def __init__(self,
+                 news_embedding_dim=400,
+                 num_attention_heads=20,
+                 query_vector_dim=200
+                ):
+        super(UserEncoder, self).__init__()
+        self.multihead_attention = MultiHeadAttention(news_embedding_dim,
+                                              num_attention_heads, 20, 20)
+        self.additive_attention = AdditiveAttention(num_attention_heads*20,
+                                                    query_vector_dim)
+        
+        self.neg_multihead_attention = MultiHeadAttention(news_embedding_dim,
+                                                         num_attention_heads, 20, 20)
+        
+    def forward(self, clicked_news_vecs):
+        multi_clicked_vectors = self.multihead_attention(
+            clicked_news_vecs, clicked_news_vecs, clicked_news_vecs
+        )
+        pos_user_vector = self.additive_attention(multi_clicked_vectors)
+        
+        user_vector = pos_user_vector
+        return user_vector
+
+class NRMS_Model(nn.Module):
+    def __init__(self, embedding_matrix):
+        super(NRMS_Model, self).__init__()
+        self.text_encoder = TextEncoder(embedding_matrix)
+        self.user_encoder = UserEncoder()
+        
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def forward(self, candidate_news, clicked_news, targets, compute_loss=True):
+        batch_size, npratio, word_num = candidate_news.shape
+        candidate_news = candidate_news.view(-1, word_num)
+        candidate_vector = self.text_encoder(candidate_news).view(batch_size, npratio, -1)
+        
+        batch_size, clicked_news_num, word_num = clicked_news.shape
+        clicked_news = clicked_news.view(-1, word_num)
+        clicked_news_vecs = self.text_encoder(clicked_news).view(batch_size, clicked_news_num, -1)
+        
+        user_vector = self.user_encoder(clicked_news_vecs)
+        
+        score = torch.bmm(candidate_vector, user_vector.unsqueeze(-1)).squeeze(dim=-1)
+        
+        if compute_loss:
+            loss = self.criterion(score, targets)
+            return loss, score
+        else:
+            return score
+        
+        
+
+class TopicEncoder(torch.nn.Module):
+    def __init__(self, num_categories=285, reduction_dim=64):
+        super(TopicEncoder, self).__init__()
+        self.num_categories = 285
+        self.word_embedding = nn.Embedding(num_categories,
+                                           reduction_dim)
+        self.mlp_head = nn.Sequential(nn.Linear(reduction_dim, reduction_dim),
+                                      nn.Dropout(),
+                                      nn.ReLU(),
+                                      nn.Linear(reduction_dim, reduction_dim))
+    def forward(self, candidate_news_topicindex):
+        """
+        Args:
+            news:
+                {
+                    "title": batch_size * num_words_title
+                }
+        Returns:
+            (shape) batch_size, word_embedding_dim
+        """
+        # batch_size, reduction_dim
+        news_vector = F.dropout(self.word_embedding(candidate_news_topicindex),
+                                training=self.training)
+        # batch_size, reduction_dim
+        final_news_vector = self.mlp_head(news_vector)
+        return final_news_vector
+    
+    def get_topic_embeddings_byindex(self, topic_index):
+        topic_indices = torch.LongTensor(topic_index)
+        topic_vector = F.dropout(self.word_embedding(topic_indices.to(next(self.word_embedding.parameters()).device)),
+                        training=True)
+        final_topic_vector = self.mlp_head(topic_vector)
+        return final_topic_vector # num_topic, reduction_dim
+    def get_all_topic_embeddings(self):
+        topic_indices = torch.LongTensor(range(0, self.num_categories)).to(next(self.word_embedding.parameters()).device)
+        # topic_indices = torch.LongTensor(topic_order)
+        topic_vector = F.dropout(self.word_embedding(topic_indices),
+                                training=True)
+        final_topic_vector = self.mlp_head(topic_vector)
+        return final_topic_vector # num_topic, reduction_dim
+
+class NRMS_Topic_Model(torch.nn.Module):
+    """
+    NRMS network.
+    Input 1 + K candidate news and a list of user clicked news, produce the click probability.
+    """
+    def __init__(self, embedding_matrix):
+        super(NRMS_Topic_Model, self).__init__()
+        self.text_encoder = TextEncoder(embedding_matrix)
+        self.user_encoder = UserEncoder()
+        self.dimmension_reduction = nn.Sequential(nn.Linear(400, 64),
+                                            nn.Dropout(),
+                                            nn.ReLU(),
+                                            nn.Linear(64, 64))
+        self.topic_encoder = TopicEncoder()
+        self.criterion = nn.CrossEntropyLoss()
+        self.all_topic_vector = None
+
+    def forward(self, candidate_news_topicindex, clicked_news, targets, compute_loss=True):
+
+        # todo: change to topic encoder
+        batch_size, npratio = candidate_news_topicindex.shape
+        candidate_news_vector = self.topic_encoder(candidate_news_topicindex).view(batch_size, npratio, -1) # batch_size, 1 + K, reduction_dim
+        
+        batch_size, clicked_news_num, word_num = clicked_news.shape
+        clicked_news = clicked_news.view(-1, word_num)
+        clicked_news_vector = self.text_encoder(clicked_news).view(batch_size, clicked_news_num, -1) # batch_size, num_clicked_news_a_user, word_embedding_dim
+        
+        user_vector = self.user_encoder(clicked_news_vector)
+        user_vector = self.dimmension_reduction(user_vector) # batch_size, reduction_dim
+        # batch_size, 1 + K
+        score = torch.bmm(candidate_news_vector, 
+                                      user_vector.unsqueeze(dim=-1)).squeeze(dim=-1)
+        if compute_loss:
+            loss = self.criterion(score, targets)
+            return loss, score
+        else:
+            return score
+
+    def get_news_vector(self, news):
+        """
+        Args:
+            news:
+                {
+                    "title": batch_size * num_words_title
+                },
+        Returns:
+            (shape) batch_size, reduction_dim
+        """
+        # batch_size, reduction_dim
+        return self.text_encoder(news)
+    
+    def get_topic_vector(self, index):
+        """
+        Args:
+            news:
+                {
+                    "title": batch_size * num
+                },
+        Returns:
+            (shape) batch_size, reduction_dim
+        """
+        # batch_size, reduction_dim
+        return self.topic_encoder(index)
+
+    def get_user_vector(self, clicked_news_vector):
+        """
+        Args:
+            clicked_news_vector: batch_size, num_clicked_news_a_user, word_embedding_dim
+        Returns:
+            (shape) batch_size, reduction_dim
+        """
+        # batch_size, word_embedding_dim
+        return self.user_encoder(clicked_news_vector)
+
+    def get_prediction(self, news_vector, user_vector):
+        """
+        Args:
+            news_vector: candidate_size, reduction_dim
+            user_vector: reduction_dim
+        Returns:
+            click_probability: candidate_size
+        """
+        # candidate_size
+        return self.click_predictor(
+            news_vector.unsqueeze(dim=0),
+            user_vector.unsqueeze(dim=0)).squeeze(dim=0)
+        
+    def get_all_topic_embedding(self):
+        """
+        Returns:
+            all_topic_vector: topic_num, reduction_dim
+        """
+
+        all_topic_vector = self.topic_encoder.get_all_topic_embeddings() # num_topic, reduction_dim
+
+        return all_topic_vector
+    
+    def get_topic_embeddings_byindex(self, topic_index):
+        """
+        Returns:
+            all_topic_vector: topic_num, reduction_dim
+        """
+
+        all_topic_vector = self.topic_encoder.get_topic_embeddings_byindex(topic_index) # num_topic, reduction_dim
+
+        return all_topic_vector
